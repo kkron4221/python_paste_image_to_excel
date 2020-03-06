cbbb0f0baf1ccbb39d777170b1c07e90eac86779
kkron4221 <knroro1877887@gmail.com>
2020-03-06 17:23:28 +0900

    Add comment

--- paste_image_auto.py
+++ paste_image_auto.py
@@ -2,6 +2,14 @@ from openpyxl import Workbook
 from openpyxl.drawing.image import Image
 import os
 
+/*Purpose: This code is automatic paste evidence task to excel sheet.
+* 1. Get file name from evidence file.
+* 2. Get file image from evidence file.
+* 3. Connect excel sheet.
+* 4. Add sheet in excel from file name.
+* 5. Paste file image to same name's sheet.
+*/
+
 wb = Workbook()
 
 excel_name = ''
