a27d1383c339767806a7d08944d3d528d0bbe020
kkron4221 <knroro1877887@gmail.com>
2020-03-10 16:09:40 +0900

    It can resize only one png

--- edit_png.py
+++ edit_png.py
@@ -1,7 +1,9 @@
 import cv2
 
 filename = "1-1-1.png"
-imgCV = cv2.imread(filename) 
-cv2.imshow("image", imgCV)
+imgCV = cv2.imread(filename)
+imgCV_width = imgCV.shape[1]
+resize_img = cv2.resize(imgCV, (imgCV_width, 700), interpolation = cv2.INTER_CUBIC)
+cv2.imshow("image", resize_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
