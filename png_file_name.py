ad8d38bb2ec211966fe4e21da44f4145b76e1a76
kkron4221 <knroro1877887@gmail.com>
2020-03-10 10:30:40 +0900

    Get folder_list

--- png_file_name.py
+++ png_file_name.py
@@ -1,13 +1,23 @@
 import glob
+import os
+import re
 
-# print(glob.__file__)
-one_png = "1"
-many_png = "3"
-png_name = "1-1-"
-png_re_name = 'C:\\Users\\kusano\\Documents\\evidence\\No.1\\' + png_name + one_png + "*"
-png_list = glob.glob(png_re_name)
-print(png_list)
-
-png_re_name = 'C:\\Users\\kusano\\Documents\\evidence\\No.1\\' + png_name + many_png + "*"
-png_list = glob.glob(png_re_name)
-print(png_list)
+evidence_folder = "C:\\Users\\username\\Documents\\evidence"
+#No.1, No.2, ...
+folder_list = os.listdir(evidence_folder)
+# 1-1-1, 1-1-2
+re_word = r'([0-9]-){2}[0-9]'
+check_list = []
+for folder_num in range(len(folder_list)):
+    # 1-1-1.png, 1-1-2.png, 1-1-2_1.png, ....
+    num_folder = evidence_folder + "\\" + folder_list[folder_num]
+    png_list = os.listdir(num_folder)
+    for png_num in range(len(png_list)):
+        re_png_name = re.match(re_word, png_list[png_num])
+        png_re_name = num_folder + "\\" + re_png_name.group() + "*"
+        # get 1-1-1 groups (list)
+        right_num_groups = glob.glob(png_re_name)
+        if right_num_groups == check_list:
+            continue
+        print(right_num_groups)
+        check_list = right_num_groups
