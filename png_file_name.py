3c9cacc5ff115ee8ea06691f87967e3db8dc9024
kkron4221 <knroro1877887@gmail.com>
2020-03-11 09:38:03 +0900

    Part of image pasting process is completed

--- png_file_name.py
+++ png_file_name.py
@@ -2,26 +2,29 @@ from openpyxl import load_workbook
 from openpyxl.drawing.image import Image
 import glob, os, re, cv2
 
-# def check_img_size(img):
-#     # low size image change other size
-#     img_check = cv2.imread(img)
-#     img_check_hight = img_check.shape[0]
-#     if img_check_hight > 700:
-#         return img
-#     resize_img = cv2.resize(img_check, (img_check.shape[1], 700), interpolation = cv2.INTER_CUBIC)
-#     return resize_img
+def check_img_size(img):
+    # low size image change other size
+    img_check = cv2.imread(img)
+    img_check_hight = img_check.shape[0]
+    if img_check_hight > 700:
+        print("Big image")
+        return 32
+    print("small image")
+    return 22
 
 def paste_image(ws2, right_num_groups):
     for num in range(len(right_num_groups)):
-        # img = check_img_size(right_num_groups[num])
-        capture_num = num * 32 + 2
+        capture_num = 2
+        if num != 0:
+            cell_size = check_img_size(right_num_groups[num - 1])
+            capture_num = num * cell_size + 2
         capture_area = 'A' + str(capture_num)
         ws2.add_image(Image(right_num_groups[num]), capture_area)
 
 def connect_excel(right_num_groups, folder_num):
     wb = load_workbook(filename = excel_name)
     sheet_name = str(folder_num + 1) + "-" + str(sheet_name_list[1]) + "-" + str(sheet_name_list[2])
-    sheet_name = sheet_name.rstrip("\'>")
+    sheet_name = sheet_name.rstrip("\'>[0-9]")
     ws2 = wb.create_sheet(title = sheet_name)
     ws2['A1'] = "※画面キャプチャーを添付する"
     paste_image(ws2, right_num_groups)
@@ -29,7 +32,7 @@ def connect_excel(right_num_groups, folder_num):
 
 # TODO: change excel's path and evidence folder path
 excel_name = 'test.xlsx'
-evidence_folder = "C:\\Users\\username\\Documents\\evidence"
+evidence_folder = "C:\\Users\\kusano\\Documents\\evidence"
 folder_list = os.listdir(evidence_folder)
 re_word = r'([0-9]-){2}[0-9]'
 check_list = []
